6c80f7e797eee0cf6847e779a5ad8427db0ea493
rodericusifo <rodericus1999@gmail.com>
2022-10-31 07:52:36 +0700

    adjust variable

--- libs/database/init.go
+++ libs/database/init.go
@@ -6,8 +6,11 @@ import (
 	postgres_repository "github.com/rodericusifo/mini-project-echo/src/repository/postgres"
 )
 
+var (
+	configApps = config.ConfigApps()
+)
+
 func InitPostgres() *postgres_repository.PostgresRepository {
-	configApps := config.ConfigApps()
 	db := config.ConfigureDatabase(configApps.Database.Postgres, constant.POSTGRES)
 	return postgres_repository.InitPostgresRepository(db)
 }
